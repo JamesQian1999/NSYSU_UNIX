8f44a45295ad519b4afbab9cc4e68810f91a5f38
jamesqian <jamesqian1999@gmail.com>
2021-04-14 00:18:17 +0800

    finish

--- PA3/PA3/checkYourOutput2.sh
+++ PA3/PA3/checkYourOutput2.sh
@@ -2,4 +2,4 @@ echo "Be patient. This might take up to 2 minutes to run..."
 (source ~/PA3/testInput2.sh)>~/PA3/testOutput2.txt
 echo Finished Running. Now checking for differences to the expected output:
 cd ~/PA3
-diff -y testOutput2.txt expectedOutput2.txt && echo You passed the basic test.
+diff testOutput2.txt expectedOutput2.txt && echo You passed the basic test.
